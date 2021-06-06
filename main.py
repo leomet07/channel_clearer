b53f5ea4cdc912ebcdd44e8010c2a9e7d37ba2d2
leomet07 <leomet07@gmail.com>
2021-06-06 11:14:44 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import os
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("We have logged in as " + str(client.user))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+    if message.content.startswith('$stop'):
+        await client.logout()
+        exit(0)
+    
+
+client.run(os.environ["token"])
